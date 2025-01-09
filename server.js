510efff46e0337571431f9bfd4409cde68ec698c
sayanbose-100 <bose.sayan2512000@gmail.com>
2025-01-09 19:20:57 +0530

    URL update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://react-client-iota.vercel.app/", // Your React app URL
+    origin: "*", // Your React app URL
     methods: ["GET", "POST"]
   }
 });
